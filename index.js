c9235422825ebd5b2d683f4ec9fef9f477eda059
sametweb <sametmutevellioglu@gmail.com>
2020-03-18 19:33:19 -0700

    package.json

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const server = require("./api/server.js");
+require("dotenv").config();
 
-const port = 5000;
+const port = process.env.PORT;
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// CI/CD pipeline stages:
+// - development
+// - test
+// - staging
+// - --- manuel gate
+// - production
